65f1702abb28779bc9340da48c671a7bd6549ab5
nbugrov-art <nbugrov1@gmail.com>
2025-02-11 23:55:37 +0300

    correct test 2

---
+++ group.py
@@ -0,0 +1,8 @@
+
+
+class Group:
+
+    def __init__(self, name):
+        self.name = name
+
+
--- testaddgroup.py
+++ testaddgroup.py
@@ -1,25 +1,31 @@
-
-
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoAlertPresentException
-from selenium.common.exceptions import NoSuchElementException
+from group import Group
 
 class AppDynamicsJob(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(5)
-        self.base_url = "https://www.google.com/"
+        self.base_url = "http://localhost/addressbook/"
         self.verificationErrors = []
         self.accept_next_alert = True
 
     def test_app_dynamics_job(self):
         driver = self.driver
         self.open_homepage(driver)
-        self.login(driver)
+        self.login(driver, "admin", "secret")
         self.open_new_contact_page(driver)
-        self.fill_contact_form(driver)
+        self.fill_contact_form(driver, Group(name="12134"))
+        self.submit_new_contact(driver)
+        self.logout(driver)
+
+    def test_2app_dynamics_job(self):
+        driver = self.driver
+        self.open_homepage(driver)
+        self.login(driver, "admin", "secret")
+        self.open_new_contact_page(driver)
+        self.fill_contact_form(driver, Group(name=""))
         self.submit_new_contact(driver)
         self.logout(driver)
 
@@ -31,29 +37,29 @@ class AppDynamicsJob(unittest.TestCase):
         # submit new contact
         driver.find_element(By.NAME, "submit").click()
 
-    def fill_contact_form(self, driver):
-        # fill contact firm
+    def fill_contact_form(self, driver, group):
+        # fill contact form
         driver.find_element(By.NAME, "firstname").click()
         driver.find_element(By.NAME, "firstname").clear()
-        driver.find_element(By.NAME, "firstname").send_keys("234234")
+        driver.find_element(By.NAME, "firstname").send_keys(group.name)
 
     def open_new_contact_page(self, driver):
         # open add contact page
         driver.find_element(By.LINK_TEXT, "add new").click()
 
-    def login(self, driver):
+    def login(self, driver, username, password):
         # login
         driver.find_element(By.XPATH, "//input[@name='user']").click()
         driver.find_element(By.XPATH, "//input[@name='user']").clear()
-        driver.find_element(By.XPATH, "//input[@name='user']").send_keys("admin")
+        driver.find_element(By.XPATH, "//input[@name='user']").send_keys(username)
         driver.find_element(By.XPATH, "//input[@name='pass']").click()
         driver.find_element(By.XPATH, "//input[@name='pass']").clear()
-        driver.find_element(By.XPATH, "//input[@name='pass']").send_keys("secret")
+        driver.find_element(By.XPATH, "//input[@name='pass']").send_keys(password)
         driver.find_element(By.XPATH, "//input[@type='submit']").click()
 
     def open_homepage(self, driver):
         # Open home page
-        driver.get("http://localhost/addressbook/")
+        driver.get(self.base_url)
 
     def tearDown(self):
         self.driver.quit()
